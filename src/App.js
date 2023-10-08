c4bf18cabd05da57268c090d8d671dc5e91f1aa4
Muzamil345 <muzammill.abbas.ss@gmail.com>
2023-10-08 16:29:41 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import AddUser from './Components/Users/AddUser';
+import UserList from './Components/Users/UserList';
+
+
+function App() {
+
+  const[usersList, setUsersList]  = useState([]);
+  
+const addUserHandler = (uName, uAge) => {
+ setUsersList((prevUsersList) => {
+  return [...prevUsersList, { name: uName, age: uAge, id:Math.random().toString()}]
+ });
+};
+
+  return (
+    <div>
+<AddUser onAddUser= {addUserHandler} />
+<UserList users = {usersList} />;
+    </div>
+  );
+}
+
+export default App;
